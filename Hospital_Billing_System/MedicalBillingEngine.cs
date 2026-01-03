202452dd0ad48d454dc4148b6da74ec89ec1aa8b
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-03 12:44:58 +0530

    [Prashant] add code implementaion for OOPs-Review

---
+++ Hospital_Billing_System/MedicalBillingEngine.cs
@@ -0,0 +1,10 @@
+﻿namespace Hospital_Billing_System
+{
+    public class MedicalBillingEngine : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Medical billing resources released");
+        }
+    }
+}
---
+++ Hospital_Billing_System/Patient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital_Billing_System
+{
+    public class Patient : Person
+    {
+        private int patientId;
+        private double emergencyCharges;
+        public int PatientId
+        {
+            get { return patientId; }
+            set { patientId = value; }
+        }
+        public double EmergencyCharges
+        {
+            get { return emergencyCharges; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Emergency charges cannot be negative.");
+                }
+                emergencyCharges = value;
+            }
+        }
+        public Patient(string name, int patientId, double emergencyCharges) : base(name)
+        {
+            PatientId = patientId;
+            EmergencyCharges = emergencyCharges;
+        }
+        public override double CalculateBill()
+        {
+            return EmergencyCharges;
+        }
+    }
+}
